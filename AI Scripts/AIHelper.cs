ef608055cadc73d93d2ccf18b11420e09e0b403c
agnesabelegu <belegu.agnesa@gmail.com>
2016-05-11 15:48:47 -0400

    Update AIHelper.cs w/ commentary
    Added a few comments to make the code more readable

--- AI Scripts/AIHelper.cs
+++ AI Scripts/AIHelper.cs
@@ -3,14 +3,14 @@
 public class AIHelper : MonoBehaviour
 {
     public float MoveSpeed = 40f;
-    public HelperStates CurrentState;
+    public HelperStates CurrentState; //What is the helper currently doing?
 
     [HideInInspector]
-    public GameObject CurrentHelperObjective;
-
+    public GameObject CurrentHelperObjective; //Where is the helper supposed to go next?
 
     void Update()
     {
+        //Simply move the helper to the current destination.
         if (CurrentState == HelperStates.MovingToLocation)
         {
             transform.position = Vector3.MoveTowards(transform.position, CurrentHelperObjective.transform.position, MoveSpeed * Time.deltaTime);
@@ -20,11 +20,12 @@ public class AIHelper : MonoBehaviour
 
     void OnColliderEnter(Collision other)
     {
+        //Once the player reaches the helper, start moving to the next location.
         if (other.gameObject.tag == "Player")
         {
             CurrentState = HelperStates.MovingToLocation;
-
         }
+        //If the helper has reached their objective, stop and wait for player.
         if (other.gameObject.tag == "Objective")
         {
             if (other.gameObject.GetComponent<Objective>().IsCurrentObjective && other.gameObject.GetComponent<Objective>().isActiveAndEnabled)
